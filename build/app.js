1c446fd193bcb2b1c8f7759fef42c744aae5e5d9
Mikael Bärlund <midas@iki.fi>
2021-02-17 16:12:58 +0200

    build

--- build/app.js
+++ build/app.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const compression_1 = __importDefault(require("compression"));
 const vessels_1 = __importDefault(require("./controllers/vessels"));
 const app = express_1.default();
 app.use(express_1.default.static('front'));
+app.use(compression_1.default());
 app.use(cors_1.default());
 app.use(express_1.default.json());
 app.use('/api/vessels', vessels_1.default);
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import compression from 'compression';
 
 import vesselsRouter from './controllers/vessels';
 const app = express();
 app.use(express.static('front'));
+app.use(compression());
 app.use(cors());
 app.use(express.json());
 
