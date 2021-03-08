6a289f48f2a68b649ab4757d73ae92570f0366e0
Mikael Bärlund <midas@iki.fi>
2021-03-08 20:57:14 +0200

    change cron to standalone task

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import { job } from './services/CronService';
 import app from './app';
 
 const PORT = process.env.PORT || 3001;
-console.log(`started job ${job.name}`);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
