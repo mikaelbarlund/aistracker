a47ef82a2f72538162b5962fe95f5e786ada5bbd
Mikael Bärlund <midas@iki.fi>
2021-03-08 21:17:51 +0200

    change cron to standalone task

--- build/app.js
+++ build/app.js
@@ -6,11 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const compression_1 = __importDefault(require("compression"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const config_1 = __importDefault(require("./config"));
 const vessels_1 = __importDefault(require("./controllers/vessels"));
 const app = express_1.default();
 app.use(express_1.default.static('front'));
 app.use(compression_1.default());
 app.use(cors_1.default());
 app.use(express_1.default.json());
+mongoose_1.default.connect(config_1.default.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => {
+    console.log('connected to MongoDB');
+}, e => console.log(e));
 app.use('/api/vessels', vessels_1.default);
 exports.default = app;
