4b79e801ecda2ff8eb7a40c81bd2bbdc35a0cacf
Mikael Bärlund <midas@iki.fi>
2021-03-08 21:56:18 +0200

    -

--- build/tasks/VesselTask.js
+++ build/tasks/VesselTask.js
@@ -8,24 +8,27 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const AisService_1 = require("../services/AisService");
 const config_1 = __importDefault(require("../config"));
 const vessel_1 = __importDefault(require("../models/vessel"));
-mongoose_1.default.connect(config_1.default.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(() => {
-    console.log('connected to MongoDB');
-    const from = new Date().getTime() - (1000 * 60 * 15);
-    AisService_1.getAisVessels(from).then(vessels => {
-        console.log(new Date(), vessels.length);
-        vessels.forEach(v => {
-            vessel_1.default.findOneAndUpdate({ mmsi: v.mmsi }, Object.assign({}, v), { new: true, upsert: true, runValidators: true }).then(newV => {
-                AisService_1.getAisFeatures(from, newV.mmsi).then(features => {
-                    const oldFeatures = immutable_1.Set(newV.features);
-                    const newFeatures = immutable_1.Set(features.features);
-                    const allFeatures = immutable_1.Set.union([oldFeatures, newFeatures]);
-                    vessel_1.default.updateOne({ mmsi: newV.mmsi }, { features: allFeatures.toArray() }, { new: true, upsert: true, runValidators: true }).then(
-                    // eslint-disable-next-line @typescript-eslint/no-empty-function
-                    _ => { }, _ => { });
+const task = () => {
+    mongoose_1.default.connect(config_1.default.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+        .then(() => {
+        console.log('connected to MongoDB');
+        const from = new Date().getTime() - (1000 * 60 * 15);
+        AisService_1.getAisVessels(from).then(vessels => {
+            console.log(new Date(), vessels.length);
+            vessels.forEach(v => {
+                vessel_1.default.findOneAndUpdate({ mmsi: v.mmsi }, Object.assign({}, v), { new: true, upsert: true, runValidators: true }).then(newV => {
+                    AisService_1.getAisFeatures(from, newV.mmsi).then(features => {
+                        const oldFeatures = immutable_1.Set(newV.features);
+                        const newFeatures = immutable_1.Set(features.features);
+                        const allFeatures = immutable_1.Set.union([oldFeatures, newFeatures]);
+                        vessel_1.default.updateOne({ mmsi: newV.mmsi }, { features: allFeatures.toArray() }, { new: true, upsert: true, runValidators: true }).then(
+                        // eslint-disable-next-line @typescript-eslint/no-empty-function
+                        _ => { }, _ => { });
+                    }, e => console.log(e));
                 }, e => console.log(e));
-            }, e => console.log(e));
-        });
-        console.log(new Date(), 'done');
+            });
+            console.log(new Date(), 'done getting vessels.');
+        }, e => console.log(e));
     }, e => console.log(e));
-}, e => console.log(e));
+};
+task();
