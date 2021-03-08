034824e29c4730c0b74126978ed5b84aed4094c1
Mikael Bärlund <midas@iki.fi>
2021-03-08 21:05:49 +0200

    change cron to standalone task

--- build/index.js
+++ build/index.js
@@ -3,10 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const CronService_1 = require("./services/CronService");
 const app_1 = __importDefault(require("./app"));
 const PORT = process.env.PORT || 3001;
-console.log(`started job ${CronService_1.job.name}`);
 app_1.default.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- build/tasks/VesselTask.js
+++ build/tasks/VesselTask.js
@@ -1,20 +1,24 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.job = void 0;
 const immutable_1 = require("immutable");
 const mongoose_1 = __importDefault(require("mongoose"));
-const node_schedule_1 = __importDefault(require("node-schedule"));
-const AisService_1 = require("./AisService");
+const AisService_1 = require("../services/AisService");
 const config_1 = __importDefault(require("../config"));
 const vessel_1 = __importDefault(require("../models/vessel"));
-mongoose_1.default.connect(config_1.default.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(() => {
-    console.log('connected to MongoDB');
-}, e => console.log(e));
-exports.job = node_schedule_1.default.scheduleJob('*/30 * * * *', () => {
+const task = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield mongoose_1.default.connect(config_1.default.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
     const from = new Date().getTime() - (1000 * 60 * 15);
     AisService_1.getAisVessels(from).then(vessels => {
         console.log(new Date(), vessels.length);
@@ -33,3 +37,5 @@ exports.job = node_schedule_1.default.scheduleJob('*/30 * * * *', () => {
         console.log(new Date(), 'done');
     }, e => console.log(e));
 });
+const value = task();
+console.log(value);
